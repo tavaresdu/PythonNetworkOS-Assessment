7cb83f72dbf6e1662908c165156ecce6f37ea075
Eduardo Corrêa <tavaresdu@gmail.com>
2018-09-21 14:42:57 -0300

    Corrections on programs 1, 4 and 5

--- 4.py
+++ 4.py
@@ -1,2 +1,5 @@
-with open('4.txt', 'r') as f:
-    print(f.read()[::-1])
+try:
+    with open('4.txt', 'r') as f:
+        print(f.read()[::-1])
+except FileNotFoundError:
+    print('Arquivo não encontrado!')
